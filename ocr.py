34487735f308c1444245ef05fea37cc9812cffa7
varadkulk <40574811+varadkulk@users.noreply.github.com>
2019-03-12 13:02:27 +0530

    aa

--- ocr.py
+++ ocr.py
@@ -1,8 +1,8 @@
 from PIL import Image
 import pytesseract
 
-img=Image.open("x.png")
+img=Image.open("b.jpg")
 
-text=pytesseract.image_to_string(img,lang='0')
+text=pytesseract.image_to_string(img,lang='eng')
 
 print(text)
