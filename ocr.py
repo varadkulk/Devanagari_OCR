8cb9da5cd03eb756758a7ec6904e020bb17d18b5
varadkulk <40574811+varadkulk@users.noreply.github.com>
2019-03-14 21:44:34 +0530

    small improvements

--- ocr.py
+++ ocr.py
@@ -1,6 +1,6 @@
 """https://github.com/tesseract-ocr/tesseract/wiki/Data-Files for reference"""
 from PIL import Image
-import pytesseract
+import pytesseract as pt
 x=-1
 while x!=0:
     print("\nEnter:")
@@ -32,7 +32,7 @@ while x!=0:
         img = Image.open(i)
         while j<=0 or j>5:
             print("\nEnter:")
-            print("1. for script output in terminal")
+            print("1. for script output in terminal (Warning: proper font required)")
             print("2. for script output to file")
             print("3. for OSD (result containing information about orientation and script detection)")
             print("4. for data (result containing box boundaries, confidences, and other information)\n")
@@ -42,13 +42,14 @@ while x!=0:
                 print("\nWrong input!!!")
             else:
                 if j==1:
-                    print(pytesseract.image_to_string(img, lang=language))
+                    print(pt.image_to_string(img, lang=language))
                 elif j==2:
-                    text = input ("Enter outputfile: ")
+                    text = input ("Enter output file: ")
                     f= open(text,"w+",encoding='utf-8')
-                    f.write(pytesseract.image_to_string(img, lang=language))
+                    f.write(pt.image_to_string(img, lang=language))
                     f.close()
+                    print("Script output to file"+text+"sucessful")
                 elif j==3:
-                    print(pytesseract.image_to_osd(img))#script info
+                    print(pt.image_to_osd(img))#script info
                 elif j==4:
-                    print(pytesseract.image_to_data(img))
+                    print(pt.image_to_data(img))
