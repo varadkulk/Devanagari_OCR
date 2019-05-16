bf46e11e7179d8a4aa19b4d8609c17e8fad8afec
varadkulk <40574811+varadkulk@users.noreply.github.com>
2019-05-16 09:30:53 +0530

    addd Copy option

--- ocr.py
+++ ocr.py
@@ -2,13 +2,16 @@
 import PIL as ig
 import pytesseract as pt
 from tkinter import *
+import pyperclip
 
 def return_value(i,language):
         img = ig.Image.open(i)
+        s= pt.image_to_string(img, lang=language)
         tex.config(state=NORMAL)
-        tex.insert(INSERT, pt.image_to_string(img, lang=language))
+        tex.insert(INSERT,s)
         tex.see(END)
         tex.config(state=DISABLED)
+        copy = Button(window, text="Copy Output", font=(font, font_size), command=pyperclip.copy(s)).pack()
         
 def submit():
         lg=str(langtk.get())
@@ -27,7 +30,7 @@ def submit():
         return_value(str(imgtk.get()),l)
         
 window = Tk()
-window.geometry('450x450+500+300')
+window.geometry('800x800+500+300')
 window.title("OCR")
 window.iconbitmap('favicon.ico')
 font = "Product Sans"
@@ -36,6 +39,7 @@ imgtk=StringVar()
 langtk = StringVar()
 language_choices = { 'English','Hindi','Marathi','Sanskrit','Gujarati'}
 langtk.set('Select Language') 
+title = Label(window, text="OCR", font=(font,20)).pack()
 lab = Label(window, text="Image file location", font=(font, font_size)).pack()
 inp = Entry(window, font=(font, font_size),textvariable=imgtk).pack()
 pop = OptionMenu(window, langtk, *language_choices)
