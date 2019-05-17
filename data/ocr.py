92bf604f23b57596ed1ada961d9b099bee408f7f
varadkulk <40574811+varadkulk@users.noreply.github.com>
2019-05-18 00:38:52 +0530

    small changes with minimization

--- data/ocr.py
+++ data/ocr.py
@@ -1,65 +1,69 @@
-"""https://github.com/tesseract-ocr/tesseract/wiki/Data-Files for reference"""
+"""https://github.com/tesseract-ocr/tesseract/wiki/Data-Files for Train data"""
+
 import PIL as ig
 import pytesseract as pt
 from tkinter import filedialog
 from tkinter import *
 import pyperclip
 
-def return_value(i,language):
-        s=''
-        try:
-                img = ig.Image.open(i)
-                
-                s= pt.image_to_string(img, lang=language)
+global n
 
-        except:
-
-                tex.config(state=NORMAL)
-                tex.insert(INSERT,"Invalid Image\n")
-                tex.see(END)
-                tex.config(state=DISABLED)
-
-                return
-
+def tex_remove_add(txt):
         tex.config(state=NORMAL)
-        tex.insert(INSERT,s)
+        tex.delete('1.0', END)
+        tex.insert(INSERT,"Output :\n")
+        tex.insert(INSERT,txt)
         tex.see(END)
         tex.config(state=DISABLED)
 
-        copy = Button(window, text="Copy Text", font=(font, font_size), command=pyperclip.copy(s)).pack()
-        
+def return_value(i,language):
+
+        s=''
+        global n
+
+        try:
+                img = ig.Image.open(i)
+                s= pt.image_to_string(img, lang=language)
+        except:
+                tex_remove_add("Invalid Image")
+                return
+
+        tex_remove_add(s)
+
+        if n==0:
+                n=1
+                copy = Button(window, text="Copy Text", font=(font, font_size), command=pyperclip.copy(s)).pack()
+
 def submit():
+ 
         if str(langtk.get())=='Select Language':
-
-                tex.config(state=NORMAL)
-                tex.insert(INSERT,"ERROR\n")
-                tex.see(END)
-                tex.config(state=DISABLED)
-
+                 tex_remove_add("Please select the language\n")
+ 
         else:
-
+ 
                 if str(langtk.get())=='English':
                         l='eng'#English
-
+ 
                 elif str(langtk.get())=='Marathi':
                         l='mar'#Marathi
-
+ 
                 elif str(langtk.get())=='Hindi':
                         l='hin'#Hindi
-
+ 
                 elif str(langtk.get())=='Sanskrit':
                         l='san'#Sanskrit
-
+ 
                 elif str(langtk.get())=='Gujarati':
                         l='guj'#Gujarati'
-
+ 
                 return_value(str(imgtk.get()),l)
-      
-def browse_button():
 
+def browse_button():
         filename = filedialog.askopenfilename()
+
         if filename!='':
                 inp.delete('0', END)
+
         inp.insert(INSERT,filename)
 
 window = Tk()#Start window
@@ -74,26 +78,26 @@ except:
         except:
                 pass
 
+n=0
+
 font = "Product Sans"
 font_size = 15
 
 imgtk = StringVar()
 langtk = StringVar()
 
-language_choices = { 'English','Hindi','Marathi','Sanskrit','Gujarati'}
-langtk.set('Select Language') 
-
 title = Label(window, text="OCR", font=(font,20)).pack()
 
 lab = Label(window, text="Image file location", font=(font, font_size)).pack()
 
 inp = Entry(window, width=100,font=(font, font_size),textvariable=imgtk)
-
 inp.config(state=NORMAL)
 inp.pack()
 
-button2 = Button(text="Browse", font=(font, font_size), command=browse_button).pack()
+browse = Button(text="Browse", font=(font, font_size), command=browse_button).pack()
 
+language_choices = { 'English','Hindi','Marathi','Sanskrit','Gujarati'}
+langtk.set('Select Language') 
 pop = OptionMenu(window, langtk, *language_choices)
 pop.config(font=(font, font_size))
 pop.pack()
--- ocr.bat
+++ ocr.bat
@@ -31,4 +31,5 @@
 ::
 ::
 ::978f952a14a936cc963da21a135fa983
+
 python data\ocr.py
